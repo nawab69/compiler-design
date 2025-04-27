672b1924c64704a25ca800718220dd14a60a5aa1
Nawab <nawabkhairuzzaman@gmail.com>
2025-04-27 23:43:16 +0600

    comment counter and grammer validation added

---
+++ comment_counter/output.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    
+
+
+    printf("Hello, World!\n"); 
+    
+    int x = 10; 
+    
+    
+
+
+
+
+    
+    return 0; 
+}
---
+++ comment_counter/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// This is a single line comment
+int main() {
+    /* This is a multi-line comment
+       that spans across
+       three lines */
+    printf("Hello, World!\n"); // Print hello world
+    
+    int x = 10; /* This is an inline comment */
+    
+    /*
+     * This is another
+     * multi-line comment
+     * with a different style
+     */
+    
+    return 0; // Return success
+}
